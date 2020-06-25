1cc5c97ec040e38020342bc287974eb94f82b123
Alon Zuman <alonzuman7@gmail.com>
2020-06-25 16:47:10 +0300

    notice the axios default base url on the config

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-// Temp for development
 // axios.defaults.baseURL = 'http://localhost:5001/vuut-react-redux/europe-west3';
-// axios.defaults.baseURL = 'https://europe-west3-vuut-react-redux.cloudfunctions.net';
-// axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.baseURL = 'https://europe-west3-vuut-react-redux.cloudfunctions.net';
 
 export const setAuthToken = token => {
   axios.defaults.headers.common['auth-token'] = token;
