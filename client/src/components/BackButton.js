d6d4202a027b3ff5e01a18cfe28c93f31440150a
Alon Zuman <alonzuman7@gmail.com>
2020-06-25 18:55:00 +0300

    fixed back button

--- client/src/components/BackButton.js
+++ client/src/components/BackButton.js
@@ -5,6 +5,6 @@ export default function BackButton() {
   const history = useHistory();
 
   return (
-    <button className='btn back-button' onClick={() => history.goBack()}>Back</button>
+    <button className='btn back-button' onClick={() => history.goBack()}><i class="fas primary back-button-icon fa-chevron-left"></i><span>Back</span></button>
   )
 }
